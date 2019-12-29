98f1c387b08ed6c87f6444d128a4869dc0ccb044
PengziPeng <527581857@qq.com>
2019-12-29 21:13:22 +0800

    react第一次

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import LoginForm from './components/LoginForm.js';
 import ReactForm from './components/ReactForm.js';
 import Zaixianxiadan from './components/Zaixianxiadan.js';
 import Pcontent from './components/Pcontent.js';
-import SignUp from './components/SignUp.js';
+
 class App extends Component {
 
   render() {
@@ -25,7 +25,7 @@ class App extends Component {
            <Route  path="/LoginForm" component={LoginForm}></Route>
            <Route  path="/Zaixianxiadan" component={Zaixianxiadan}></Route>
            <Route  path="/Pcontent" component={Pcontent}></Route>
-           <Route  path="/Signup" component={SignUp}></Route>
+         
          </Switch>
  
          </BrowserRouter>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,42 +5,98 @@ import {Link} from 'react-router-dom'
 
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}}
 
- render(){
+
+
+
+        
+ render(){ 
+     
+    window.onload=function(){
+    createCode(4);    
+   }
+
+   //生成验证码的方法
+   function createCode(length) {
+       var code = "";
+       var codeLength = parseInt(length); //验证码的长度
+       var checkCode = document.getElementById("checkCode");
+       ////所有候选组成验证码的字符，当然也可以用中文的
+       var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+       'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+       'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); 
+       //循环组成验证码的字符串
+       for (var i = 0; i < codeLength; i++)
+       {
+           //获取随机验证码下标
+           var charNum = Math.floor(Math.random() * 62);
+           //组合成指定字符验证码
+           code += codeChars[charNum];
+       }
+       if (checkCode)
+       {
+           //为验证码区域添加样式名
+           checkCode.className = "code";
+           //将生成验证码赋值到显示区
+           checkCode.innerHTML = code;
+       }
+   }
+   
+
+//    function ulogin() {
+				
+//     var userName = document.getElementById("uName").value;
+//     var userPass = document.getElementById("uPass").value;
+    
+//     if (userPass == "123" && userName == "帅气的小鹏") {
+//         alert("登陆成功");
+//         window.location.href='欢迎界面2.html'
+//     } 
+//     else if (userName == "" || userName == null) {
+//         alert("用户名不能为空");
+//         return false;
+//     } 
+//     else if (userPass == "" || userPass == null) {
+//         alert("密码不能为空");
+//         return false;
+//     } 
+//     else if (userPass != "root" || userName != "root") {
+//         alert("用户名或密码错误");
+//         return false;
+//     } 
+//     else{
+//         return true;
+//     }
+        
+// }
 
 
     return(
         <div>
-            <div class="div1">
-                <img>
-                </img>
-                <p class="div2">欢迎登陆</p>
-            </div>
-            <div class="div4">
-                <table>
-                <div class="div5">
-            <table align="center" class="div6">
-                <tr>
-                    <td class="div13">邮箱/用户名/学号</td>
-                </tr>
-                    <input type="text" placeholder="请输入用户名" class="div7"></input>
-                    <input type="text" placeholder="请输入密码" class="div8"></input>
-                    
-                    
-                        <td class="div9">密码
-                    </td>
-                        <td class="div10">
-                            自动登录
-                        </td>
-                      <a href ="www.baidu.com" class="div11">忘记密码？</a>
-                    
-                    <input type="checkbox" class="div12"></input>
-                    <button class="div14">登录</button>
-                    <Link to='SignUp'><button class="div15">免费注册>></button></Link>
-            </table>
-            </div>
-            </table>
-            </div>
+            <img class="div102"></img>
+            <p class="div101" >欢迎登录</p>
+            <p class="div103">用户名/学号</p>
+            <p class="div104">密码</p>
+             <p class="div120">验证码： </p>
+            <p class="div105">忘记秘密？</p>
+            <input type="checkbox" class="div106"></input> 
+            <input type="text" placeholder="请输入用户名" class="div108" id="uName"></input>
+            <input type="text" placeholder="请输入密码" class="div109" id="uPass"></input>  
+            <input type="text" id="inputCode" placeholder="请输入验证码" class="div121"/>
+        
+            <td> <div id="checkCode"  class="code"  onclick="createCode(4)" ></div></td>
+            {/* <td> <span onclick="createCode(4)">看不清换一张</span></td> */}
+
+            <p class="div107">自动登录</p>
+            <button class="div110"  onclick="" >登录</button>
+          
+           
+           
+      
+           
         </div>
     )
  }
